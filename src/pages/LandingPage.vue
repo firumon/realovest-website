c0220ef59a9cb885dc75d770df3ccd3ba3492746
Firose Hussain <github@firumon.com>
2025-03-19 12:27:29 +0530

    Landing page

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="bg-secondary text-primary" padding>
+    <div class="text-center">
+      <div class="text-h2 text-bold">REALOVEST DEVELOPERS</div>
+      <div class="text-h3 text-bold">PRIVATE LIMITED</div>
+    </div>
+    <div class="row q-mx-none text-h5 q-py-xl">
+      <div class="col-2 text-left">&nbsp;</div>
+      <div class="col-4 text-left text-bold">CIN: U43299KL2025PTC092221</div>
+      <div class="col-4 text-right text-bold">PH: 9539272435</div>
+      <div class="col-2 text-right">&nbsp;</div>
+    </div>
+    <div class="q-pa-lg q-mx-xl bg-primary text-secondary text-center text-bold text-h5">
+      831(2), PUTHEN VEETTIL, MOON PIECE, OTTATHENGU W<br>CHAVAKKAD, THRISSUR - 680506
+    </div>
+    <div class="text-center q-mt-xl">
+      <div class="text-h3 text-bold">SANDALWOOD</div>
+      <div class="text-h4 text-bold">GROUP FARMING PROJECT</div>
+      <div class="q-my-md"><q-img width="40%" style="max-width:40%" src="https://iwst.icfre.gov.in/database/SIS/taxonomy/image/tree.JPG" /></div>
+      <div class="text-h5 text-bold">Pombilaya, Nellaya Panchayath, Palakkad District</div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
